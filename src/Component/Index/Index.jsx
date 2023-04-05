4fa5dd2ecf25277a75f316e64ae7435c2ddba18c
Luigi3357 <luigi33579@gmail.com>
2023-04-05 19:49:42 -0300

    first commit

---
+++ src/Component/Index/Index.jsx
@@ -0,0 +1,12 @@
+import { React, useState } from "react";
+
+
+export default function Index() {  
+    
+    return (
+        <div>
+            <img src="https://us.123rf.com/450wm/bunphot001/bunphot0012201/bunphot001220100457/180585590-color-blanco-y-gris-abstracto-fondo-de-dise%C3%B1o-moderno-con-forma-geom%C3%A9trica-ilustraci%C3%B3n.jpg?ver=6" alt="" />
+        </div>
+    )
+}
+
